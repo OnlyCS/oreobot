5e5d71372385a3e2b9f4972fbb08dc01f0ce47ad
Angad Tendulkar <angad.tendulkar@gmail.com>
2023-09-18 10:45:59 -0400

    fix some problems with errors [skip ci]

--- src/events/error.rs
+++ src/events/error.rs
@@ -7,31 +7,28 @@ pub async fn handle(
         poise::FrameworkError::Command { error, ctx } => {
             let mut embed = embed::default(
                 &ctx,
-                if let CommandError::RuntimeWarning { .. } = error {
-                    embed::EmbedStatus::Warning
+                if matches!(error, CommandError::RuntimeWarning { .. }) {
+                    EmbedStatus::Warning
                 } else {
-                    embed::EmbedStatus::Error
+                    EmbedStatus::Error
                 },
             );
 
             match error {
                 CommandError::RuntimeWarning { title, description } => {
                     embed.title(format!("{} > Warning > {}", ctx.command().name, title));
-                    embed.description(format!("Warning in command `/{}`:\n{}\n\nDon't worry, this warning has been appropriately handled and the bot will continue to function normally", ctx.command().name, description));
+                    embed.description(format!("Warning in command `{}{}`:\n{}\n\nDon't worry, this warning has been appropriately handled and the bot will continue to function normally", if ctx.command().slash_action.is_some() { "/" } else {""}, ctx.command().name, description));
                 }
                 CommandError::RuntimeError { title, description } => {
                     embed.title(format!(
                         "{} > Error > {}",
-                        ctx.command().name.capitalize_first_letter(),
+                        ctx.command().name.display_case(),
                         title
                     ));
                     embed.description(format!("Error in command `/{}`:\n{}\n\nDon't worry, this error has been appropriately handled and the bot will continue to function normally", ctx.command().name, description));
                 }
                 _ => {
-                    embed.title(format!(
-                        "{} > Error",
-                        ctx.command().name.capitalize_first_letter()
-                    ));
+                    embed.title(format!("{} > Error", ctx.command().name.display_case()));
                     embed.description(format!("Error in command `/{}`:\n{}\n\nDon't worry, this error has been appropriately handled and the bot will continue to function normally", ctx.command().name, error.to_string()));
                 }
             }
