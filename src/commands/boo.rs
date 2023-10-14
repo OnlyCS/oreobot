a1ad962eecf9cc918f9061c25ccdbc2fc31d13da
Angad Tendulkar <angad.tendulkar@gmail.com>
2023-10-14 17:30:01 -0400

    Ctx defer

--- src/commands/boo.rs
+++ src/commands/boo.rs
@@ -3,5 +3,6 @@ use crate::prelude::*;
 #[poise::command(context_menu_command = "Boo (for Helen)")]
 pub async fn boo(ctx: Context<'_>, message: serenity::Message) -> Result<(), CommandError> {
   message.reply(&ctx, "wooooooow. this is the most unfunny joke i have ever had the displeasure of reading. try again later.").await?;
+  ctx.defer_ephemeral().await?;
   Ok(())
 }
